2bcb5e287801323cdd9a5da0a921dc62689f2490
Yi-Huan Chan <hubert_chan@trend.com.tw>
2013-09-29 14:35:00 +0800

    Use class to wrap logics

--- indievox-mp3-rename.py
+++ indievox-mp3-rename.py
@@ -12,49 +12,70 @@ MP3_REGEX = \
     r'^(?P<author>.+?)-(?P<album>.+?)-(?P<track>\d+)-(?P<song>.+?\.mp3)$'
 
 
+class IndievoxSong(object):
+    track = None
+    song = None
+
+    def __init__(self, track, song):
+        self.track = track
+        self.song = song
+
+    def make_filename(self):
+        return "%02d %s" % (int(self.track), self.song)
+
+
+class IndievoxSongMeta(object):
+    indievox_song = None
+    author = None
+    album = None
+    filename = None
+
+    def __init__(self, author, album, indievox_song, filename):
+        self.author = author
+        self.album = album
+        self.indievox_song = indievox_song
+        self.filename = filename
+
+    @classmethod
+    def match(cls, filename):
+        return re.match(MP3_REGEX, unicode(filename, 'utf-8'),
+                        flags=re.UNICODE)
+
+    @classmethod
+    def from_filename(cls, filename):
+        mp3file = cls.match(filename)
+        if not mp3file:
+            return None
+
+        author, album, track, song = (mp3file.group('author'),
+                                      mp3file.group('album'),
+                                      mp3file.group('track'),
+                                      mp3file.group('song'))
+
+        indievox_song = IndievoxSong(track, song)
+        instance = cls(author, album, indievox_song, filename)
+
+        return instance
+
+
 def indievox_renamer(music_dir):
-    def make_mp3files(filenames):
-        def match(file):
-            return re.match(MP3_REGEX, unicode(filename, 'utf-8'),
-                            flags=re.UNICODE)
-        return [(match(filename), filename)
-                for filename in filenames if match(filename)]
+    def rename(mp3files):
+        for mp3file in mp3files:
+            dirname = os.path.join(music_dir, mp3file.author, mp3file.album)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
 
-    def make_mp3data(mp3files):
-        mp3data = {}
-        for mp3file, filename in mp3files:
-            author, album, track, song = (mp3file.group('author'),
-                                          mp3file.group('album'),
-                                          int(mp3file.group('track')),
-                                          mp3file.group('song'))
+            filename = mp3file.filename
+            source = os.path.join(music_dir, filename)
+            target = os.path.join(dirname,
+                                  mp3file.indievox_song.make_filename())
 
-            mp3data.setdefault(author, {})
-            mp3data[author].setdefault(album, [])
-            mp3data[author][album].append((track, song, filename))
+            os.rename(source, target)
 
-        return mp3data
-
-    def rename(mp3data):
-        def make_filename(song_info):
-            return "%02d %s" % song_info[0:2]
-
-        for (author, album_data) in mp3data.iteritems():
-            for (album, song_infos) in album_data.iteritems():
-                dirname = os.path.join(music_dir, author, album)
-
-                if not os.path.exists(dirname):
-                    os.makedirs(dirname)
-
-                for song_info in song_infos:
-                    filepath = song_info[2]
-                    source = os.path.join(music_dir, filepath)
-                    target = os.path.join(dirname, make_filename(song_info))
-
-                    os.rename(source, target)
-
-    mp3files = make_mp3files(os.listdir(music_dir))
-    mp3data = make_mp3data(mp3files)
-    rename(mp3data)
+    mp3files = (IndievoxSongMeta.from_filename(filename)
+                for filename in os.listdir(music_dir)
+                if IndievoxSongMeta.from_filename(filename))
+    rename(mp3files)
 
 
 def main():
